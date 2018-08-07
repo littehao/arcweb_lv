6a6e096921b6cd52a86d49a8607969e00db310e7
Harris <>
2018-08-07 18:23:05 +0800

    长跑

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -15,7 +15,7 @@ let layouts = {
 let resolvedLayouts = {}
 
 export default {
-  head: {"title":"arcweb_ssr_lv","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"My awe-inspiring Nuxt.js project"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+  head: {"title":"LV","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"My awe-inspiring Nuxt.js project"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
   render(h, props) {
     const loadingEl = h('nuxt-loading', { ref: 'loading' })
     const layoutEl = h(this.layout || 'nuxt')
--- pages/classproduct/index.vue
+++ pages/classproduct/index.vue
@@ -1,15 +1,55 @@
 <template>
-    <div>
-      产品类型
+    <div class="lv-classproduct">
+        <div class="lv-classproduct-content1">
+            <div class="lv-classproduct-banner">
+                <img src="~assets/images/classproduct/banner.png" alt="">
+            </div>
+            <div class="classproduct-part">
+                <Container>
+                    <product-part1></product-part1>
+                </Container>
+            </div>
+        </div>
+        <div class="lv-classproduct-content2">
+            <div class="classproduct-part">
+                <Container>
+                    <product-part2></product-part2>
+                </Container>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
+import productPart1 from '~/components/classproduct/part1'
+import productPart2 from '~/components/classproduct/part2'
+export default {
+    name: "index",
+    components:{
+        productPart1,
+        productPart2,
     }
+}
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.lv-classproduct{
+    width:100%;
+    min-width:1200px;
+    .lv-classproduct-content1{
+        width:100%;
+        background:url(~assets/images/classproduct/bg.png) no-repeat center bottom #1C1C23;
+        background-size:100%;
+        .lv-classproduct-banner{
+            width:100%;
+            img{
+                width:100%;
+                display: block;
+            }
+        }
+    }
+    .lv-classproduct-content2{
+        background-color:#1C1C23;
+    }
+}
 </style>
