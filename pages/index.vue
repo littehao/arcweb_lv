409c8ce6ac903e88977891466ad103b5f68b9dec
Peter <1914571088@qq.com>
2018-10-12 22:48:41 +0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="lv-home">
         <Mhome v-if="currentView == 'mobile'"></Mhome>
+
+
         <Lhome v-else-if="currentView == 'default'"></Lhome>
     </div>
 </template>
 
 
 <script type="text/javascript">
-import { mapGetters } from 'vuex'
-import Lhome from './home/index'
-import Mhome from './mhome/index'
+import { mapGetters } from 'vuex';
+import Lhome from './home/index';
+import Mhome from './mhome/index';
 export default {
     layout (context) {
         return context.userAgent;
